8d808b790ec16ce5f4004ff2b59687f2add37eb2
SammyWin <kingstonru@yandex.ru>
2019-07-18 01:17:51 +0300

    hometask_3 complete fixed

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -35,7 +35,7 @@ class Bender(var status : Status = Status.NORMAL, var question: Question = Quest
                 else
                 {
                     status = status.nextStatus()
-                    "Это не правильный ответ\n${question.question}" to status.color
+                    "Это неправильный ответ\n${question.question}" to status.color
                 }
             }
         }
