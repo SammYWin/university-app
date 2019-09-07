ab9011f4bdb80b8701a45d3a8ceedc6c65d2b174
SammyWin <kingstonru@yandex.ru>
2019-09-08 00:27:06 +0300

    hometask_5 video completed

--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/GroupViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/GroupViewModel.kt
@@ -1,19 +1,33 @@
 package ru.skillbranch.devintensive.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import android.view.View
+import androidx.lifecycle.*
 import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.UserItem
 import ru.skillbranch.devintensive.repositories.GroupRepository
+import kotlin.coroutines.coroutineContext
 
 class GroupViewModel : ViewModel(){
+    private val query = mutableLiveData("")
     private val groupRepository = GroupRepository
-    val userItems = mutableLiveData(loadUsers())
-    val selectedItems = Transformations.map(userItems){users -> users.filter { it.isSelected }}
+    private val userItems = mutableLiveData(loadUsers())
+    private val selectedItems = Transformations.map(userItems){users -> users.filter { it.isSelected }}
 
     fun getUsersData() : LiveData<List<UserItem>>{
-        return userItems
+        val result = MediatorLiveData<List<UserItem>>()
+
+        val FilterF = {
+            val queryStr = query.value!!
+            val users = userItems.value!!
+
+            result.value = if(queryStr.isEmpty()) users
+                            else users.filter { it.fullName.contains(queryStr, true) }
+        }
+
+        result.addSource(userItems){FilterF.invoke()}
+        result.addSource(query){FilterF.invoke()}
+
+        return result
     }
 
     fun getSelectedData() : LiveData<List<UserItem>> = selectedItems
@@ -27,7 +41,22 @@ class GroupViewModel : ViewModel(){
 
     }
 
-    private fun loadUsers(): List<UserItem> = groupRepository.loadUsers().map{it.toUserItem()}
+    fun handleRemoveChip(userId: String) {
+        userItems.value = userItems.value!!.map {
+            if (it.id == userId)
+                it.copy(isSelected = false)
+            else it
+        }
+    }
 
+    fun handleSearchQuery(text: String?) {
+        query.value = text
+    }
+
+    fun handleCreateGroup() {
+        groupRepository.createChat(selectedItems.value)
+    }
+
+    private fun loadUsers(): List<UserItem> = groupRepository.loadUsers().map{it.toUserItem()}
 
 }
